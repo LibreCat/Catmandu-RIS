0d1627b49842e8fe0b336e2c5c556b66d4ef1be9
vpeil <vitali.peil@uni-bielefeld.de>
2015-07-13 21:31:55 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# NAME
+
+Catmandu::RIS -  Catmandu modules for working with RIS data
+
+# STATUS
+
+[![Build Status](https://travis-ci.org/LibreCat/Catmandu-RIS.svg?branch=master)](https://travis-ci.org/LibreCat/Catmandu-RIS)
+[![Coverage](https://coveralls.io/repos/LibreCat/Catmandu-RIS/badge.png?branch=master)](https://coveralls.io/r/LibreCat/Catmandu-RIS)
+[![CPANTS kwalitee](http://cpants.cpanauthors.org/dist/Catmandu-RIS.png)](http://cpants.cpanauthors.org/dist/Catmandu-RIS)
+
+# Author
+
+Nicolas Steenlant
+
+# CONTRIBUTOR
+
+Vitali Peil
+
+# COPYRIGHT
+
+Copyright 2015- Vitali Peil
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
+
+[Catmandu](https://metacpan.org/pod/Catmandu), [Catmandu::Exporter](https://metacpan.org/pod/Catmandu::Exporter), [Catmandu::Importer](https://metacpan.org/pod/Catmandu::Importer)
+
+## for other bibliographic formats
+
+[Catmandu::BibTeX](https://metacpan.org/pod/Catmandu::BibTeX)
---
+++ lib/Catmandu/RIS.pm
@@ -0,0 +1,51 @@
+package Catmandu::RIS;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Catmandu::RIS -  Catmandu modules for working with RIS data
+
+=begin markdown
+
+# STATUS
+
+[![Build Status](https://travis-ci.org/LibreCat/Catmandu-RIS.svg?branch=master)](https://travis-ci.org/LibreCat/Catmandu-RIS)
+[![Coverage](https://coveralls.io/repos/LibreCat/Catmandu-RIS/badge.png?branch=master)](https://coveralls.io/r/LibreCat/Catmandu-RIS)
+[![CPANTS kwalitee](http://cpants.cpanauthors.org/dist/Catmandu-RIS.png)](http://cpants.cpanauthors.org/dist/Catmandu-RIS)
+
+=end markdown
+
+=head1 Author
+
+Nicolas Steenlant
+
+=head1 CONTRIBUTOR
+
+Vitali Peil
+
+=head1 COPYRIGHT
+
+Copyright 2015- Vitali Peil
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+L<Catmandu>, L<Catmandu::Exporter>, L<Catmandu::Importer>
+
+=head2 for other bibliographic formats
+
+L<Catmandu::BibTeX>
+
+=cut
